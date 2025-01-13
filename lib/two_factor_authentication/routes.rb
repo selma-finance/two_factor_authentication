5e9fceaf3ea4db8ac1575f3dc94bea2a86d54ab9
Jouni Renfors <jouni@renfors.net>
2025-01-13 13:07:58 +0200

    Rails 8 support

--- lib/two_factor_authentication/routes.rb
+++ lib/two_factor_authentication/routes.rb
@@ -2,12 +2,18 @@ module ActionDispatch::Routing
   class Mapper
     protected
 
-      def devise_two_factor_authentication(mapping, controllers)
-        resource :two_factor_authentication, :only => [:show, :update, :resend_code], :path => mapping.path_names[:two_factor_authentication], :controller => controllers[:two_factor_authentication] do
-          collection do
-            post :resend_code
-          end
-        end
-      end
+    def devise_two_factor_authentication(mapping, controllers)
+      path = mapping.path_names[:two_factor_authentication]
+      controller = controllers[:two_factor_authentication]
+
+      resource :two_factor_authentication,
+               only: [:show, :update],
+               path: path,
+               controller: controller
+
+      post "#{path}/resend_code",
+           to: "#{controller}#resend_code",
+           as: "#{mapping.name}_two_factor_authentication_resend_code"
+    end
   end
 end
